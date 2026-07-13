c2e3ae73eaac1e2ead22295efc89622eb5d1da4c
Erick <erickarroyo1312@protonmail.com>
2026-07-13 11:19:44 -0400

    Fixed vignette

--- R/chk_vol.R
+++ R/chk_vol.R
@@ -3,16 +3,16 @@
 #' @description Summarize volume transferred from each Source Well in a picklist
 #'
 #' @param pl Picklist table. The names of the columns are assumed to be "Src_Well" and "Transfer_Volume"
+#'
+#' @param plate_col Name of column containing source plate names. Default is "SrcPlateName"
+#'
+#' @param sample_col Name of column containing sample name. Default is "SampleName"
 #' 
 #' @return A tibble with the total volume transferred from each source well
 #'
 #' @export
 #'
 #' @importFrom magrittr %>%
-#'
-#' @examples
-#'
-#' testmath(4)
 #'
 chk_vol <- function(pl, plate_col = "SrcPlateName", sample_col = "SampleName"){
     dplyr::group_by_at(pl, c("SrcWell", plate_col, sample_col )) %>%
